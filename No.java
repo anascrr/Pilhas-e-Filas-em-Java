41b6ba1f220d17f477249cf7821a90e01cba895b
Ana Júlia da Silva Corrêa <ajuliascorrea@gmail.com>
2025-03-18 20:03:50 -0300

    Update No.java

--- No.java
+++ No.java
@@ -1,25 +1,25 @@
 public class No {
-    private int dado;
-    private No proximo;
+    private int dado; // variável dado
+    private No proximo; // referência para o próximo nó da lista
 
-    public No(int dado) {
+    public No(int dado) { // construtor
         this.dado = dado;
-        this.proximo = null;
+        this.proximo = null; // iniciado com null indicando que está vazio
     }
 
-    public int obterDado() {
+    public int obterDado() { // armazenar valor do nó
         return dado;
     }
 
-    public void definirDado(int dado) {
+    public void definirDado(int dado) { // definir valor no nó
         this.dado = dado;
     }
 
-    public No obterProximo() {
+    public No obterProximo() { // refência para o próximo nó da lista
         return proximo;
     }
 
-    public void definirProximo(No proximo) {
+    public void definirProximo(No proximo) { // define o próximo nó na sequência
         this.proximo = proximo;
     }
 }
