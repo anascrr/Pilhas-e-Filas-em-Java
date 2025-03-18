0d0b5213b0d1332ff49c2a53f3e7bd20c56c59e5
Ana Júlia da Silva Corrêa <ajuliascorrea@gmail.com>
2025-03-18 20:11:25 -0300

    Update PilhaSequencial.java

--- PilhaSequencial.java
+++ PilhaSequencial.java
@@ -1,23 +1,23 @@
 public class PilhaSequencial implements InterfacePilha {
-	private int[] vetor;
-	private int topo;
+	private int[] vetor; // variável vetor para armazenar a pilha
+	private int topo; // índice de topo, último elemento inserido
 
-	public PilhaSequencial() {
-		this.vetor = new int[20];
-		this.topo = -1;
+	public PilhaSequencial() { // construtor
+		this.vetor = new int[20]; // definir tamanho fixo
+		this.topo = -1; // pilha vazia
 	}
 
 	@Override
-	public void push(int dado) {
-		this.topo++;
-		this.vetor[topo] = dado;
+	public void push(int dado) { // empilhar
+		this.topo++; // incrementa a variável topo para apontar para a próxima posição, move o topo para a próxima posição disponível
+		this.vetor[topo] = dado; // insere o dado na nova posição do topo
 	}
 
 	@Override
-	public int pop() {
-		int aux = vetor[topo];
-		topo--;
-		return aux;
+	public int pop() { // desempilhar
+		int aux = vetor[topo]; // auxiliar para guardar o valor do elemento do topo
+		topo--; // decrementa o topo, posição anterior pois o último foi removido
+		return aux; // retorna o valor removido
 	}
 
 	@Override
@@ -34,11 +34,11 @@ public class PilhaSequencial implements InterfacePilha {
 	}
 
 	@Override
-	public String toString() {
-		String elementos = new String();
-		for (int i = 0; i <= topo; i++) {
-			elementos += vetor[i] + " ";
+	public String toString() { // representação
+		String elementos = new String(); // string vazia para armazenar elementos da pilha
+		for (int i = 0; i <= topo; i++) { // loop para percorrer toda a pilha
+			elementos += vetor[i] + " "; // constrói uma string com todos os elemntos da pilha, pegando na posição
 		}
-		return "Pilha Sequencial (max 20): [ " + elementos + "]";
+		return "Pilha Sequencial (max 20): [ " + elementos + "]"; // retorna representação
 	}
 }
