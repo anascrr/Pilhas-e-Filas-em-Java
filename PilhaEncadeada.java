93114a7eaa807a2894b99fb596b09070bd0fda79
Ana Júlia da Silva Corrêa <ajuliascorrea@gmail.com>
2025-03-18 20:07:44 -0300

    Update PilhaEncadeada.java

--- PilhaEncadeada.java
+++ PilhaEncadeada.java
@@ -1,24 +1,23 @@
 public class PilhaEncadeada implements InterfacePilha {
-
-	No noTopo = null;
-	int totalDeElementos = 0;
+	No noTopo = null; // pilha inicia vazia
+	int totalDeElementos = 0; // contador inicia vazio
 
 	@Override
-	public void push(int dado) {
-		No novoNo = new No(dado);
+	public void push(int dado) { // empilhadar dado
+		No novoNo = new No(dado); // novo nó criado com o valor recebido por parâmetro
 		novoNo.definirProximo(this.noTopo);
 		this.noTopo = novoNo;
 		this.totalDeElementos++;
 	}
 
 	@Override
-	public int pop() {
-		if (this.noTopo != null) {
-			int dado = this.noTopo.obterDado();
-			this.noTopo = this.noTopo.obterProximo();
-			return dado;
+	public int pop() { // desempilhar
+		if (this.noTopo != null) { // se o topo não estiver vazio
+			int dado = this.noTopo.obterDado(); // armazena o valor do dado no topo
+			this.noTopo = this.noTopo.obterProximo(); // atualiza o topo da pilha para o próximo nó
+			return dado; // retorna o dado
 		}
-		return -1;
+		return -1; // erro
 	}
 
 	@Override
@@ -35,13 +34,13 @@ public class PilhaEncadeada implements InterfacePilha {
 	}
 
 	@Override
-	public String toString() {
-		String elementos = new String();
-		No noAtual = this.noTopo;
-		while (noAtual != null) {
-			elementos = elementos + noAtual.obterDado() + " ";
-			noAtual = noAtual.obterProximo();
+	public String toString() { // representação
+		String elementos = new String(); // string para armazenar os elementos da pilha
+		No noAtual = this.noTopo; // ponteiro para percorrer a pilha, começando do topo
+		while (noAtual != null) { // enquanto houve nós, percorra
+			elementos = elementos + noAtual.obterDado() + " "; // adiciona o valor do nó à string
+			noAtual = noAtual.obterProximo(); // avança para o próximo
 		}
-		return "Pilha Encadeada: [ " + elementos + "]";
+		return "Pilha Encadeada: [ " + elementos + "]"; // retorna a representação
 	}
 }
